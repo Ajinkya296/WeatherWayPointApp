fb20c34d93ad7d07cb9ad6c57b6fe2dde6205f44
Ajinkya296 <ajinkyagawali95@gmail.com>
2018-09-09 18:42:50 -0400

    Base

---
+++ test.js
@@ -0,0 +1,26 @@
+/*var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBNmXtV61112WJDnpctz8jBKc_ByTiUUf4'
+});
+
+// Geocode an address.
+googleMapsClient.geocode({
+  address: '1600 Amphitheatre Parkway, Mountain View, CA'
+}, function(err, response) {
+  if (!err) {
+    console.log(response.json.results);
+  }
+});
+*/
+let apiKey = '6d597adf5b2e9548c15a8d856286086b';
+let city = 'buffalo';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+const request = require('request');
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
