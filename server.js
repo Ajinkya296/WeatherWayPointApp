59e0cc3aec9d65c938d1a5b7fe340336b9bb1db5
Ajinkya296 <ajinkyagawali95@gmail.com>
2018-09-09 20:27:36 -0400

    Fix await issue

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 // content of index.js
+let apiKey = '6d597adf5b2e9548c15a8d856286086b';
+let url = `http://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=${apiKey}`
+const request = require('request');
 
+
+function getWeatherInCity(city)
+{
+  var info
+  request(url+`&q=${city}`, function (err, response, body) {
+    if(err){
+      console.log('error:', error);
+    }
+    else {
+      let weather = JSON.parse(body)
+      let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      info    = weather.main.temp;
+
+    }
+  });
+console.log(info)
+return info
+}
 
 
 const express = require('express')
@@ -12,8 +33,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
 })
-app.post('/submit',(request, response) => {
-  response.send('Server recieved : ' + request.body.firstname + '  ' + request.body.lastname )
+app.post('/submit',async (request, response) => {
+  let temp = await getWeatherInCity(request.body.city)
+  console.log('--'+temp)
+  response.send('Its'+ temp + ' in ' +request.body.city)
 })
 app.listen(port, (err) => {
   if (err) {
